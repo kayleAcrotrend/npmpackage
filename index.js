59417087013852c7d844f97c3a96a28068da01d6
kayleAcrotrend <98522685+kayleAcrotrend@users.noreply.github.com>
2022-05-19 17:04:59 +0100

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = function program(aws, siteBucket) {
+    const indexContent = `<html><head>
+      <title>Hello S3</title><meta charset="UTF-8">
+      </head>
+      <body><p>Hello, world!</p><p>Made with ❤️ with <a href="https://pulumi.com">Pulumi</a></p>
+      </body></html>
+      `
+
+    let object = new aws.s3.BucketObject('index', {
+      bucket: siteBucket,
+      content: indexContent,
+      contentType: 'text/html; charset=utf-8',
+      key: 'index.html'
+    })
+
+    function publicReadPolicyForBucket(bucketName) {
+      return {
+        Version: '2012-10-17',
+        Statement: [{
+          Effect: 'Allow',
+          Principal: '*',
+          Action: [ 's3:GetObject' ],
+          Resource: [ `arn:aws:s3:::${bucketName}/*` ]
+        }]
+      }
+    }
+
+    let bucketPolicy = new aws.s3.BucketPolicy('bucketPolicy', {
+      bucket: siteBucket.bucket,
+      policy: siteBucket.bucket.apply(publicReadPolicyForBucket)
+    })
+}
